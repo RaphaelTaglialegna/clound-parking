61c1efac9471be7f981536ddc6508b596d615466
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-11-16 06:19:32 -0300

    Initial Commit

---
+++ src/test/java/br/floreia/parking/CloundParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.floreia.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloundParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
