3b78f1109f999767141f5b15720008fb5ff93751
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-11-19 17:49:07 -0300

    Método Create

--- src/main/java/br/floreia/parking/sevice/ParkingService.java
+++ src/main/java/br/floreia/parking/sevice/ParkingService.java
@@ -1,5 +1,6 @@
 package br.floreia.parking.sevice;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,5 +29,17 @@ public class ParkingService {
 
   public List<Parking> findAll() {
     return parkingMap.values().stream().collect(Collectors.toList());
+  }
+
+  public Parking findById(String id ) {
+    return parkingMap.get(id);
+  }
+
+  public Parking create(Parking parkingCreate) {
+    String uuid = getUUID();
+    parkingCreate.setId(uuid);
+    parkingCreate.setEntryDate(LocalDateTime.now());
+    parkingMap.put(uuid, parkingCreate);
+    return parkingCreate;
   }
 }
